9a89267109afb63b30c43d0474efc5978aa5223e
Camille Tsai <a0983936190@gmail.com>
2021-12-09 15:55:12 +0800

    取除phone多寫的0

--- member-edit.php
+++ member-edit.php
@@ -85,7 +85,7 @@ try {
                             <div class="col-md-5">
                                 <label for="phone" class="form-label">電話</label>
                                 <input type="text" class="form-control" id="phone" name="member_phone"
-                                       placeholder="0<?= $value["member_phone"] ?>">
+                                       placeholder="<?= $value["member_phone"] ?>">
                             </div>
                             <div class="col-md-5">
                                 <label for="email" class="form-label">信箱</label>
