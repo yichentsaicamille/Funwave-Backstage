f060316546274f303db4989cdb50390cbe58b59d
Alice Yen <gp40424@gmail.com>
2021-12-10 09:59:59 +0800

    jquery 引用拿掉 rel="stylesheet"

--- public/css.php
+++ public/css.php
@@ -4,7 +4,7 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js">
 <!-- fontawesome -->
 <link rel="stylesheet" href="fontawesome-free-5.15.4-web/css/all.css">
 <!-- style -->
