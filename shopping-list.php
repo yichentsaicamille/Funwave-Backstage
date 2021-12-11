4dce237b9a233405cd61664bd2c69752aa6ebc35
Ian Hsieh <energy0702@gmail.com>
2021-12-12 00:21:13 +0800

    shopping-list title

--- shopping-list.php
+++ shopping-list.php
@@ -45,7 +45,7 @@ if(isset($_POST['add_cart'])){
 <!doctype html>
 <html lang="en">
 <head>
-    <title>Product list</title>
+    <title>Shopping list</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
